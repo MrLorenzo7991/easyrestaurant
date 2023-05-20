f62bfb63fda4ee0a51b1c738c1a00694f10a8acd
Gaetano793 <129846714+Gaetano793@users.noreply.github.com>
2023-05-20 21:09:38 +0200

    fermi2

--- src/main/java/Model/Comanda.java
+++ src/main/java/Model/Comanda.java
@@ -3,11 +3,15 @@ package Model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Comanda {
 
