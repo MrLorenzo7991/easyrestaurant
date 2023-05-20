f6537b2ef35fb41ce84a8bac706b3c9b7a0849ee
MrLorenzoContrader <l.miccio@contrader.it>
2023-05-20 19:24:07 +0200

    Initial commit

---
+++ src/main/java/com/easyrestaurant/easyrestaurant/EasyrestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.easyrestaurant.easyrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyrestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyrestaurantApplication.class, args);
+	}
+
+}
